1b0323d53a56d63cfcb950db0034c8322dfbce31
alzekiel09 <aixonairashii@gmail.com>
2018-01-19 01:53:31 +0800

    added firebase credential

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-  	apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+  	apiKey: "AIzaSyAf7_x0guazlbR9pjqfkUhxwfOr2wJmelw",
+    authDomain: "firestore-crud-of-users-9a145.firebaseapp.com",
+    databaseURL: "https://firestore-crud-of-users-9a145.firebaseio.com",
+    projectId: "firestore-crud-of-users-9a145",
+    storageBucket: "firestore-crud-of-users-9a145.appspot.com",
+    messagingSenderId: "697289795538"
   }
 };
